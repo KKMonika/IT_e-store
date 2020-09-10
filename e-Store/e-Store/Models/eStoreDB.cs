382a55c48604d30370e9c019dd6c5d3667387f13
Monika Korunoska <monikakorunoskaa@gmail.com>
2020-09-10 18:13:57 +0200

    Controllers and Views

---
+++ e-Store/e-Store/Models/eStoreDB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace e_Store.Models
+{
+    public class eStoreDB: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
